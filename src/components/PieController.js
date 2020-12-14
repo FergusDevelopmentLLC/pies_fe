0c1505c181101c20fe480bbfcea6e9a365bf2b8a
Will Carter <will.carter@fergusllc.com>
2020-12-14 16:58:26 -0700

    refactor, useHistory not necessary

--- src/components/PieController.js
+++ src/components/PieController.js
@@ -5,28 +5,26 @@ import PieControls from './PieControls'
 import { fetchPie } from '../actions/pieActions'
 import { deletePie } from '../actions/pieActions'
 import { useSelector, useDispatch} from 'react-redux'
-import { useHistory } from "react-router"
 
 const PieController = (props) => {
 
-  const pie = useSelector(state => state.pieReducer.pie)
   const dispatch = useDispatch()
-  const history = useHistory()
+  const pie = useSelector(state => state.pieReducer.pie)
   
-  const getPie = () => Object.keys(pie).length > 0 ? <Pie pieData={ pie } /> : null
+  const getPieComponent = () => Object.keys(pie).length > 0 ? <Pie pieData={ pie } /> : null
   const getPieControls = () => Object.keys(pie).length > 0 ? <PieControls pieData={ pie } /> : null
 
   useEffect(() => {
     dispatch(fetchPie(props.id))
-  }, [])
+  },[props.id])
   
   return (
     <div className='pie-container'>
-      { getPie() }
+      { getPieComponent() }
       { getPieControls() }
       <div className="delete-pie-wrapper"><button onClick={ () => {
         dispatch(deletePie(pie))
-        history.push("/pies")
+        props.history.push("/pies")
       } }>Delete Pie</button></div>
       
     </div>
